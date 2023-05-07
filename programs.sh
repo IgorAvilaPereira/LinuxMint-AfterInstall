a0d0e3d47f31fcca4d2479188d40d4f4cca3a377
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-05-06 23:15:34 -0300

    Update programs.sh

--- programs.sh
+++ programs.sh
@@ -111,7 +111,7 @@ sudo apt autoremove -y
 sudo apt list --upgradable
 
 # vscode
-sudo apt install software-properties-common apt-transport-https wget
+sudo apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add –
 sudo add-apt-repository --yes "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt install code
